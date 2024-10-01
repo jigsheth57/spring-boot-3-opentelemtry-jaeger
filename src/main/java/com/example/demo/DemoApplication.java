5cc92b51c930559842914a081132e0adf7d1a175
Jig Sheth <jsheth@vmware.com>
2024-10-01 13:16:14 -0500

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -24,50 +24,45 @@ public class DemoApplication {
   @Value("${spring.application.name}")
   private String applicationName;
 
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	//Init OpenTelemetry
-	@Bean
+  // Init OpenTelemetry
+  @Bean
   public OpenTelemetry initOpenTelemetry(@Value("${otel.exporter.otlp.endpoint}") String jaegerEndpoint) {
 
-    // Include required service.name resource attribute on all spans and metrics 
+    // Include required service.name resource attribute on all spans and metrics
     // autowire's not working in boot v3.0.x or v3.1.x
-    Resource resource =
-    Resource.getDefault()
+    Resource resource = Resource.getDefault()
         .merge(Resource.builder().put("service.name", applicationName).build());
 
-		// Export traces to Jaeger over OTLP
-    OtlpGrpcSpanExporter jaegerOtlpExporter =
-        OtlpGrpcSpanExporter.builder()
-            .setEndpoint(jaegerEndpoint)
-            .setTimeout(30, TimeUnit.SECONDS)
-            .build();
+    // Export traces to Jaeger over OTLP
+    OtlpGrpcSpanExporter jaegerOtlpExporter = OtlpGrpcSpanExporter.builder()
+        .setEndpoint(jaegerEndpoint)
+        .setTimeout(30, TimeUnit.SECONDS)
+        .build();
 
-		// Set to process the spans by the Jaeger Exporter
-    SdkTracerProvider tracerProvider =
-        SdkTracerProvider.builder()
-            .addSpanProcessor(BatchSpanProcessor.builder(jaegerOtlpExporter).build())
-            .setResource(resource)
-            .build();
-    OpenTelemetrySdk openTelemetry =
-        OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
+    // Set to process the spans by the Jaeger Exporter
+    SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+        .addSpanProcessor(BatchSpanProcessor.builder(jaegerOtlpExporter).build())
+        .setResource(resource)
+        .build();
+    OpenTelemetrySdk openTelemetry = OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).build();
 
     // it's always a good idea to shut down the SDK cleanly at JVM exit.
     Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
 
     return openTelemetry;
   }
-
-	// @Bean
-	// public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
-	// 	return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
-	// }
+  // @Bean
+  // public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${otel.exporter.otlp.endpoint}")
+  // String url) {
+  // return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
+  // }
 }
